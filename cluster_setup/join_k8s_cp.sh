48ccd797f672a377d83305823e86d65f14c0df13
sleevetosleeve <sleevetosleeve@gmail.com>
2024-10-03 22:04:27 +0200

    Control plane join script

---
+++ cluster_setup/join_k8s_cp.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [[ $EUID -eq 0 ]]; then
+  echo "This script must NOT be run as root" 1>&2
+  exit 1
+fi
+
+SCRIPT_DIR="$(dirname "$0")"
+
+#install stuff
+. $SCRIPT_DIR/node.sh
+
+#join cluster
+sudo $SCRIPT_DIR/controlplane.sh
+
+#setup kubectl
+mkdir -p $HOME/.kube
+cat $SCRIPT_DIR/kubeconfig > $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+#bash compleation
+$SCRIPT_DIR/bash_compleation.sh
